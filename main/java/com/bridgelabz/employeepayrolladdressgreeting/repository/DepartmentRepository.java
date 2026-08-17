5926cc647befe075ab7a6fbed6e181826949f582
Saravana20-S <iamsaravanan1347@gmail.com>
2026-08-17 13:52:50 +0530

    Added Dept repo code

---
+++ main/java/com/bridgelabz/employeepayrolladdressgreeting/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.employeepayrolladdressgreeting.repository;
+
+import com.bridgelabz.employeepayrolladdressgreeting.model.Department;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+
+}
