6f388d7c4a977aa97e582c9ddff6af1b90781039
Saravana20-S <iamsaravanan1347@gmail.com>
2026-08-17 14:03:19 +0530

    Main Logic file added

---
+++ main/java/com/bridgelabz/employeepayrolladdressgreeting/EmployeepayrolladdressgreetingApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrolladdressgreeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeepayrolladdressgreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeepayrolladdressgreetingApplication.class, args);
+	}
+
+}
